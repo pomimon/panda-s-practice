c998b3a5a3c189a7e6afe9ddb8316b029e60fe8b
pomimon <bright.dusk1649@fastmail.com>
2025-02-20 12:54:47 +0400

    Read dataframe from SQL tables and merge

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from sqlalchemy import create_engine
 DB_FILE = "/Users/mango/projects/data-analytics/sql-practice/03-pokemon.db"
 DB_PATH = f"sqlite+pysqlite:///{DB_FILE}"
 
+
 def main():
 
+    pokemon_df = pd.read_sql_table('pokemon', DB_PATH)
+    pokemonTypes_df = pd.read_sql_table('types', DB_PATH)
 
+    pokemon_df.merge(pokemonTypes_df, on='id')
+
+    print(pokemon_df)
 
 if __name__ == "__main__":
     main()
